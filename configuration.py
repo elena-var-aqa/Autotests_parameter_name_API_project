facfaa0ebe73e72e5c25db1549ec96f0aea66b2d
Elena Varaksina <168302442+elena-var-aqa@users.noreply.github.com>
2026-05-24 20:42:02 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://af05cfbe-26f1-4115-a753-279a42922d80.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://af05cfbe-26f1-4115-a753-279a42922d80.serverhub.services.ru"
 
 CREATE_KIT_PATH = "/api/v1/kits"
 CREATE_USER_PATH = "/api/v1/users/"
